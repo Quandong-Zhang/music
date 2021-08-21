d7f887c49d2a15c12601d03bfc3370d63f158969
Quandong-Zhang <78526012+Quandong-Zhang@users.noreply.github.com>
2021-08-21 21:08:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 from mido import MidiFile
 
-mid = MidiFile('input.mid')
+mid1 = MidiFile('input.mid')
+for i, track in enumerate(mid1.tracks):
+    #print('Track {}: {}'.format(i, track.name))
+    for i in range(len(track)):
+        msgs=track[i]
+        if msgs.type == 'note_on':
+            print(msgs)
