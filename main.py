6145fb438929c05db0017d1ea27fad615d047984
Quandong-Zhang <78526012+Quandong-Zhang@users.noreply.github.com>
2021-08-22 22:11:31 +0800

    last update

--- main.py
+++ main.py
@@ -22,25 +22,25 @@ tim=0
 #
 mid = MidiFile(type=1)
 track = MidiTrack()
-track.append(mido.MetaMessage('set_tempo', tempo=bpm2tempo(75), time=0))
+track.append(mido.MetaMessage('set_tempo', tempo=bpm2tempo(80), time=0))
 mid.tracks.append(track)
 #
 for i, track1 in enumerate(mid1.tracks):
     for i in range(len(track1)):
         msgs=track1[i]
         if msgs.type=='note_on' or msgs.type=='note_off':
-            tim+=msgs.time
+            tim=tim+msgs.time
             if msgs.type == 'note_on':
-                if msgs.note!=last_note:
-                    print('aaaaa')
+                #if msgs.note!=last_note:
+                if 0==0:
+                    #print('aaaaa')
                     #track.append(Message('note_on',note=msgs.note,channel=0,program= 40,time=msgs.time))
-                    track.append(Message('note_on', note=msgs.note ,velocity=64, time=0))
+                    track.append(Message('note_on',channel=6 ,note=msgs.note ,velocity=64, time=0))
                     #track.append(Message('note_on', note=64, velocity=64,program=40 , time=32))
-                    track.append(Message('note_off', note=msgs.note, velocity=64, time=tim))
+                    track.append(Message('note_off', channel=6 ,note=msgs.note, velocity=64, time=tim*3))
                     tim=0
-                    print("a")
-                last_note=msgs.note
-                print(last_note)
+                    #last_note=msgs.note
+                #print(last_note)
 
 #mid.tracks.append(track)
 mid.save('new_song.mid')
